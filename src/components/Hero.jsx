ef05d8d62d108c0bac013c2b9ac1b466f37d12ea
Sumit Sharma <sumits2700@gmail.com>
2024-01-22 11:15:01 +0530

    fixing resume download bug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import myPhoto from '../assets/myPhoto.jpeg'
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
-import { Link } from 'react-scroll';
+import resume from '../../public/resume/Sumit Sharma Resume.pdf'
 
 function Hero() {
   return (
@@ -30,7 +30,7 @@ function Hero() {
             </a>
           </div>
           
-            <a href="\resume\Sumit Sharma Resume.pdf" download={'Sumit_Sharma_Resume'}>
+            <a href={resume} download={'Sumit_Sharma_Resume'}>
           <button className='btn my-5'>Download CV</button>
           </a>
           
